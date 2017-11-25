81937687e3c81faf21fea3ae9a6061bf88d6259b
xabertum <mohatar@gmail.com>
2017-11-25 12:02:56 +0100

    Jumbotron Media Queries added

--- js/navbar_hide.js
+++ js/navbar_hide.js
@@ -6,11 +6,29 @@ $(document).ready(function () {
         var currScrollTop = $(this).scrollTop();
         if (scroll >= banner_height && currScrollTop > lastScrollTop) {
             $("#banner").hide();
-        } else {
+        } else if (scroll == 0) {
             $("#banner").show();
         }
         lastScrollTop = currScrollTop;
 
     });
+
+});
+
+
+/* MENU COLOR CHANGE WHEN COLLAPSE */
+
+$(document).ready(function () {
+
+    $(".collapse").on("show.bs.collapse", function () {
+        $("#navscroll").css("transition", "background-color 0.1s ease-in");
+        $("#navscroll").css("background-color", "gray");
+    });
+
+    $(".collapse").on("hide.bs.collapse", function () {
+        $("#navscroll").css("transition", "background-color 0.5s ease-out");
+        $("#navscroll").css("background-color", "transparent");
+    });
+
 
 });
